2eb3a7182fb3047300992fe0500565bb54a29817
Henrry <hosu4549@gmail.com>
2022-12-10 16:57:37 +0900

    [add] kubernetes init update datadog key delete

--- datadog/01_install_datadog_values.sh
+++ datadog/01_install_datadog_values.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-helm install datadog-agent -f datadog-values.yaml --set datadog.site='us5.datadoghq.com' --set datadog.apiKey=f90251964232846919a49cdccccaaa70 datadog/datadog -n datadog
+helm install datadog-agent -f datadog-values.yaml --set datadog.site='{site}' --set datadog.apiKey='{datadog.apikey}' datadog/datadog -n datadog
 
